f07ca53094c1a17ed4859f9a6779bd02c1f42e70
Rodrigo Argumedo <rodrigo.argumedo@outlook.com>
2015-11-01 19:05:07 -0600

    Initial commit

---
+++ lib/writer/version.rb
@@ -0,0 +1,3 @@
+module Writer
+  VERSION = "0.1.0"
+end
---
+++ spec/writer_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Writer do
+  it 'has a version number' do
+    expect(Writer::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
